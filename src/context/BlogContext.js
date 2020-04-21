ac6d15a8f8203234c481d508477cb03de6d18753
arthurs14 <arthurcs426@gmail.com>
2020-04-20 17:44:00 -0700

    useReducer can now add posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,19 +1,36 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useReducer } from 'react';
 
 const BlogContext = createContext();
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
 
-  const addBlogPost = post => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` },
-    ]);
+export const BlogProvider = ({ children }) => {
+  // with use state:
+  // const [blogPosts, setBlogPosts] = useState([]);
+
+  // const addBlogPost = post => {
+  //   setBlogPosts([
+  //     ...blogPosts,
+  //     { title: `Blog Post #${blogPosts.length + 1}` },
+  //   ]);
+  // };
+
+  // with useReducer:
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
   };
 
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
     </BlogContext.Provider>
   );
